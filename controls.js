f856b6f098aa6841a0a6a1e4d76d4aaa38ef8063
Harshilathwani20 <harshilathwanioffice@gmail.com>
2025-02-14 01:07:42 +0530

    Lecture 1

---
+++ controls.js
@@ -0,0 +1,47 @@
+class Controls{
+    constructor(){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        this.#addKeyboardListeners();
+    }
+// This # Used For Define This Is A Private Method It Mean No One Able To Call It From Outside of this Control Class
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;      
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+            console.table(this);
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+            console.table(this);
+        }
+    }
+}
